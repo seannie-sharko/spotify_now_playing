45b0f29a3c140e67fbbf200fe0d77844b2ef3a61
Sean-Brooks <42978289+Sean-Brooks@users.noreply.github.com>
2022-01-31 17:07:23 -0500

    Initial commit

---
+++ creds.py
@@ -0,0 +1,5 @@
+scope = 'user-read-currently-playing'
+username = 'username'
+client_id='aaaaaaaaaaaaaaaa1234567890123456'
+client_secret='aaaaaaaaaaaaaaaa1234567890123456'
+redirect_uri='http://127.0.0.1:9999'
