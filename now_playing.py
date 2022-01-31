d1cf30bed713170f6068da1e2a0c4da8a9eaa2d4
Sean-Brooks <42978289+Sean-Brooks@users.noreply.github.com>
2022-01-31 17:12:08 -0500

    Initial commit

---
+++ now_playing.py
@@ -0,0 +1,15 @@
+import spotipy
+import spotipy.util as util
+import creds
+
+token = util.prompt_for_user_token(creds.username, creds.scope, creds.client_id, creds.client_secret, creds.redirect_uri)
+spotify = spotipy.Spotify(auth=token)
+current_track = spotify.current_user_playing_track()
+
+print("Now Playing: " + \
+f"{current_track['item']['artists'][0]['name']}" + \
+" - " + \
+"'" + f"{current_track['item']['name']}" + "'" + \
+" | " + \
+" Album: " + \
+f"{current_track['item']['album']['name']}")
