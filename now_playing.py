f4eb9473115ba4875fd2292d04a1a68ba98a2da9
Sean-Brooks <42978289+Sean-Brooks@users.noreply.github.com>
2022-01-31 20:13:43 -0500

    Added exception if client isn't found

--- now_playing.py
+++ now_playing.py
@@ -6,10 +6,13 @@ token = util.prompt_for_user_token(creds.username, creds.scope, creds.client_id,
 spotify = spotipy.Spotify(auth=token)
 current_track = spotify.current_user_playing_track()
 
-print("Now Playing: " + \
-f"{current_track['item']['artists'][0]['name']}" + \
-" - " + \
-"'" + f"{current_track['item']['name']}" + "'" + \
-" | " + \
-" Album: " + \
-f"{current_track['item']['album']['name']}")
+try:
+    print("Now Playing: " + \
+    f"{current_track['item']['artists'][0]['name']}" + \
+    " - " + \
+    "'" + f"{current_track['item']['name']}" + "'" + \
+    " | " + \
+    " Album: " + \
+    f"{current_track['item']['album']['name']}")
+except TypeError:
+    print("Now Playing: No song found... :(")
