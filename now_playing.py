8d1e808a2ebf292b19fc5da323490686e329672f
Sean-Brooks <42978289+Sean-Brooks@users.noreply.github.com>
2022-02-07 13:09:48 -0500

    Update now_playing.py

--- now_playing.py
+++ now_playing.py
@@ -1,53 +1,69 @@
 import spotipy
 import spotipy.util as util
 import creds
+from datetime import datetime
 
+token = util.prompt_for_user_token(
+        creds.username,
+        'user-read-currently-playing',
+        creds.client_id,
+        creds.client_secret,
+        creds.redirect_uri
+)
+
+"""
+Build OAuth token for your Spotify app
+
+Parameters:
+    username = 'username'
+    scope = 'user-read-currently-playing'
+    client_id='aaaaaaaaaaaaaaaa1234567890123456'
+    client_secret='aaaaaaaaaaaaaaaa1234567890123456'
+    redirect_uri='http://127.0.0.1:9999'
+"""
+
+spotify = spotipy.Spotify(auth=token)
+current_track = spotify.current_user_playing_track()
+ 
 def now_playing():
-
-    token = util.prompt_for_user_token(
-            creds.username,
-            creds.scope,
-            creds.client_id,
-            creds.client_secret,
-            creds.redirect_uri
-        )
-
-    """
-    Build OAuth token for your Spotify app
-
-    Parameters:
-        username = 'username'
-        scope = 'user-read-currently-playing'
-        client_id='aaaaaaaaaaaaaaaa1234567890123456'
-        client_secret='aaaaaaaaaaaaaaaa1234567890123456'
-        redirect_uri='http://127.0.0.1:9999'
-    """
-
-    spotify = spotipy.Spotify(auth=token)
-    current_track = spotify.current_user_playing_track()
-
     try:
         while current_track['is_playing'] == True:
-            print("Now Playing: " + \
-            f"{current_track['item']['artists'][0]['name']}" + \
-            " - " + \
-            "'" + f"{current_track['item']['name']}" + "'" + \
-            " | " + \
-            " Album: " + \
-            f"{current_track['item']['album']['name']}")
+            playing()
             break
         else:
-            print("Song Paused")
+            paused()
     except TypeError:
         print("Now Playing: No song found... :(")
 
-        # Artist Name
-        #{current_track['item']['artists'][0]['name']}
+def playing():
+    print(f"Now Playing: " + \
+    f"{current_track['item']['artists'][0]['name']}" + \
+    " - " + \
+    "'" + f"{current_track['item']['name']}" + "'" + \
+    " | " + \
+    " Album: " + \
+    f"{current_track['item']['album']['name']}")
 
-        # Track Name
-        #{current_track['item']['name']}
+def paused():
+    a=str(current_track['timestamp'])
+    a=a[0:10]
+    a=str(datetime.fromtimestamp(int(a)))
+    a=a[11:19]
 
-        # Album Name
-        #{current_track['item']['album']['name']}
+    print(f"Song Paused @ " + a + ":")
+    print(f"{current_track['item']['artists'][0]['name']}" + \
+    " - " + \
+    "'" + f"{current_track['item']['name']}" + "'" + \
+    " | " + \
+    " Album: " + \
+    f"{current_track['item']['album']['name']}")
+
+    """
+    Parameters:
+        a = '1644100566336'
+        a = '1644100566'
+        a = '2022-02-05 17:36:06'
+        a = '17:36:06'
+    """
 
 now_playing()
