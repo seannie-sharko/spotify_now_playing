c874899ec21e8310ecafe3697b44e7e1298116a9
Sean <42978289+seannie-sharko@users.noreply.github.com>
2025-02-22 18:36:23 -0500

    Update now_playing.py

--- now_playing.py
+++ now_playing.py
@@ -1,75 +1,64 @@
-"""
-This is a simple script to display the current track
-you're listening to in Spotify using Spotipy.
----Spotipy: https://github.com/plamere/spotipy
-"""
-
+from datetime import datetime
+import os
+import sys
 import spotipy
 import spotipy.util as util
+sys.path.insert(0, f'/Users/{os.getenv("USER")}/scripts/python')
 import creds
-from datetime import datetime
 
-token = util.prompt_for_user_token(
-        creds.username,
+def get_spotify_token(cred_file):
+    """Retrieve the OAuth token for Spotify authentication."""
+    return util.prompt_for_user_token(
+        cred_file.username,
         'user-read-currently-playing',
-        creds.client_id,
-        creds.client_secret,
-        creds.redirect_uri
-)
+        cred_file.client_id,
+        cred_file.client_secret,
+        cred_file.redirect_uri
+    )
 
-"""
-Build OAuth token for your Spotify app
+def get_current_track(spotify):
+    """Fetch the current track from the Spotify API."""
+    return spotify.current_user_playing_track()
 
-Parameters:
-    username = 'username'
-    scope = 'user-read-currently-playing'
-    client_id='aaaaaaaaaaaaaaaa1234567890123456'
-    client_secret='aaaaaaaaaaaaaaaa1234567890123456'
-    redirect_uri='http://127.0.0.1:9999'
-"""
+def format_track_info(track):
+    """Format the track information to display artist, track name, and album name."""
+    artist_name = track['item']['artists'][0]['name']
+    track_name = track['item']['name']
+    album_name = track['item']['album']['name']
+    return f"{artist_name} - '{track_name}' | Album: {album_name}"
 
-spotify = spotipy.Spotify(auth=token)
-current_track = spotify.current_user_playing_track()
+def display_playing_info(track):
+    """Display information when a song is currently playing."""
+    track_info = format_track_info(track)
+    print(f"Now Playing: {track_info}")
 
-def now_playing():
-    try:
-        while current_track['is_playing'] == True:
-            playing()
-            break
-        else:
-            paused()
-    except TypeError:
-        print("Now Playing: No song found... :(")
+def display_paused_info(track):
+    """Display information when the song is paused, including the timestamp."""
+    timestamp = format_timestamp(track['timestamp'])
+    track_info = format_track_info(track)
+    print(f"Song Paused @ {timestamp}:")
+    print(track_info)
 
-def playing():
-    print(f"Now Playing: " + \
-    f"{current_track['item']['artists'][0]['name']}" + \
-    " - " + \
-    "'" + f"{current_track['item']['name']}" + "'" + \
-    " | " + \
-    " Album: " + \
-    f"{current_track['item']['album']['name']}")
+def format_timestamp(timestamp):
+    """Format the timestamp into HH:MM:SS format."""
+    timestamp_str = str(timestamp)[:10]  # Remove milliseconds part
+    return str(datetime.fromtimestamp(int(timestamp_str)))[11:19]
 
-def paused():
-    a=str(current_track['timestamp'])
-    a=a[0:10]
-    a=str(datetime.fromtimestamp(int(a)))
-    a=a[11:19]
+def now_playing(cred_file):
+    """Check if a song is playing or paused and display relevant information."""
+    token = get_spotify_token(cred_file)
+    spotify = spotipy.Spotify(auth=token)
+    current_track = get_current_track(spotify)
+    if current_track is None:
+        print("No song found... :(")
+        return
+    if current_track.get('is_playing', False):
+        display_playing_info(current_track)
+    else:
+        display_paused_info(current_track)
 
-    print(f"Song Paused @ " + a + ":")
-    print(f"{current_track['item']['artists'][0]['name']}" + \
-    " - " + \
-    "'" + f"{current_track['item']['name']}" + "'" + \
-    " | " + \
-    " Album: " + \
-    f"{current_track['item']['album']['name']}")
+def main(cred_file):
+    now_playing(cred_file)
 
-    """
-    Parameters:
-        a = '1644100566336'
-        a = '1644100566'
-        a = '2022-02-05 17:36:06'
-        a = '17:36:06'
-    """
-
-now_playing()
+if __name__ == "__main__":
+    main(creds)
